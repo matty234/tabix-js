33b60ebdfc0b8209a81e988f59e0cc22f3f028dc
Matthew Brown <matthew@bargrove.com>
2019-08-19 16:12:55 +0200

    Export chunkise too

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ const TabixIndexedFile = require('./tabixIndexedFile')
 const TBI = require('./tbi')
 const CSI = require('./csi')
 const unzip = require('./unzip')
+const unzipChunk = require('./unzip')
 
-module.exports = { TabixIndexedFile, TBI, CSI, unzip }
+module.exports = { TabixIndexedFile, TBI, CSI, unzip, unzipChunk }
