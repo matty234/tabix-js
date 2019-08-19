419f68c0c60eebed8ebef8b0b9b4ae481a2b7211
Matthew Brown <matthew@bargrove.com>
2019-08-19 16:14:05 +0200

    mmm

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";var TabixIndexedFile = require('./tabixIndexedFile');
 var TBI = require('./tbi');
 var CSI = require('./csi');
+var unzip = require('./unzip');
+var unzipChunk = require('./unzip');
 
-module.exports = { TabixIndexedFile: TabixIndexedFile, TBI: TBI, CSI: CSI };
+module.exports = { TabixIndexedFile: TabixIndexedFile, TBI: TBI, CSI: CSI, unzip: unzip, unzipChunk: unzipChunk };
