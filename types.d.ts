e83c9d4ee1a5aef0ef75aef1a0db535ea6b359ff
Matthew Brown <matthew@bargrove.com>
2019-08-19 15:31:08 +0200

    Add Some More Types

--- types.d.ts
+++ types.d.ts
@@ -1,54 +1,77 @@
+import { GenericFilehandle } from "generic-filehandle";
+
 export class CSI {
-    constructor(_ref: any);
+	constructor(_ref: any);
 
-    blocksForRange(_x5: any, _x6: any, _x7: any, _x8: any, ...args: any[]): any;
+	blocksForRange(ref: string, start: number, end: number, _x8: options): List<Chunk>;
 
-    getMetadata(_x3: any, ...args: any[]): any;
+	getMetadata(_x3: any, ...args: any[]): any;
 
-    lineCount(_x: any, _x2: any, ...args: any[]): any;
+	lineCount(_x: any, _x2: any, ...args: any[]): any;
 
-    parse(): any;
+	parse(): any;
 
-    parseAuxData(bytes: any, offset: any, auxLength: any): any;
+	parseAuxData(bytes: any, offset: any, auxLength: any): any;
 
-    parsePseudoBin(bytes: any, offset: any): any;
+	parsePseudoBin(bytes: any, offset: any): any;
+}
 
+class TBIOptions {
+    filehandle: GenericFilehandle
 }
 
 export class TBI {
-    constructor(_ref: any);
+	constructor(opts: TBIOptions & any);
 
-    blocksForRange(_x5: any, _x6: any, _x7: any, _x8: any, ...args: any[]): any;
+	async blocksForRange(ref: string, start: number, end: number, _x8: options): List<Chunk>;
 
-    getMetadata(_x3: any, ...args: any[]): any;
+	getMetadata(_x3: any): any;
 
-    lineCount(_x: any, _x2: any, ...args: any[]): any;
+	lineCount(_x: any, _x2: any): any;
 
-    parse(): any;
+	parse(): any;
 
-    parsePseudoBin(bytes: any, offset: any): any;
-
+	parsePseudoBin(bytes: any, offset: any): any;
 }
 
 export class TabixIndexedFile {
-    constructor(_ref: any, ...args: any[]);
+	constructor(_ref: any, ...args: any[]);
 
-    checkLine(_ref3: any, regionRefName: any, regionStart: any, regionEnd: any, line: any): any;
+	checkLine(_ref3: any, regionRefName: any, regionStart: any, regionEnd: any, line: any): any;
 
-    getHeader(...args: any[]): any;
+	getHeader(...args: any[]): any;
 
-    getHeaderBuffer(_x8: any, ...args: any[]): any;
+	getHeaderBuffer(_x8: any, ...args: any[]): any;
 
-    getLines(_x3: any, _x4: any, _x5: any, _x6: any, ...args: any[]): any;
+	getLines(_x3: any, _x4: any, _x5: any, _x6: any, ...args: any[]): any;
 
-    getMetadata(_x7: any, ...args: any[]): any;
+	getMetadata(_x7: any, ...args: any[]): any;
 
-    getReferenceSequenceNames(_x9: any, ...args: any[]): any;
+	getReferenceSequenceNames(_x9: any, ...args: any[]): any;
 
-    lineCount(_x10: any, _x11: any, ...args: any[]): any;
+	lineCount(_x10: any, _x11: any, ...args: any[]): any;
 
-    readChunk(_x15: any, _x16: any, ...args: any[]): any;
+	readChunk(_x15: any, _x16: any, ...args: any[]): any;
 
-    renameRefSeq(refName: any): any;
+	renameRefSeq(refName: any): any;
+}
 
+export class Chunk {
+	/**
+  * @param {VirtualOffset} minv
+  * @param {VirtualOffset} maxv
+  * @param {number} bin
+  * @param {number} [fetchedSize]
+  */
+	constructor(minv: VirtualOffset, maxv: VirtualOffset, bin: number, fetchedSize: number);
+}
+
+export class VirtualOffset {
+	constructor(blockPosition: number, dataPosition: number);
+
+	static fromBytes(bytes: Int8Array, offset?: number, bigendian?: number);
+
+	compareTo(b: any);
+
+	static min(...args);
 }
